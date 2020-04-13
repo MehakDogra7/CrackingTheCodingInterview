8a28552c996f6134fe195a6173407fc95af602ca
vinayak.chaturvedi96@gmail.com <vinayak.chaturvedi96@gmail.com>
2020-04-14 02:06:37 +0530

    Code Refactor

--- java/src/IsPermPalindrome.java
+++ java/src/IsPermPalindrome.java
@@ -2,6 +2,7 @@ public class IsPermPalindrome {
 
     public static void main(String[] args) {
         String s1 = "taco act";
+        String s2 = "radar";
         System.out.println("Is permutation palindrome of " + s1 + ": " + isPermPalindrome(s1));
     }
 
