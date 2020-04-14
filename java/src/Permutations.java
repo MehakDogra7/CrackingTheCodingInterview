e5f94ddcf866471171f3a6e3cc26218dac7c0657
vinayak.chaturvedi96@gmail.com <vinayak.chaturvedi96@gmail.com>
2020-04-14 19:23:56 +0530

    Adding Permutations

---
+++ java/src/Permutations.java
@@ -0,0 +1,20 @@
+public class Permutations {
+
+    public static void main(String[] args) {
+        String str = "abc";
+
+        System.out.println("All permutations of string " + str + " are: ");
+        printPermutation(str, "");
+    }
+
+    private static void printPermutation(String str, String prefix) {
+        if (str.length() == 0) {
+            System.out.println(prefix);
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String rem = str.substring(0, i) + str.substring(i + 1);
+                printPermutation(rem, prefix + str.charAt(i));
+            }
+        }
+    }
+}
