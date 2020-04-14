acb7eb2a8ab924a076fbd389d4da31083ff54746
vinayak.chaturvedi96@gmail.com <vinayak.chaturvedi96@gmail.com>
2020-04-14 20:00:30 +0530

    Adding StringCompression

---
+++ java/src/StringCompression.java
@@ -0,0 +1,26 @@
+public class StringCompression {
+
+    public static void main(String[] args) {
+        String str1 = "aabbbcccccd";
+        String str2 = "aabcd";
+
+        System.out.println("Compression of " + str1 + " is: " + compress(str1));
+        System.out.println("Compression of " + str2 + " is: " + compress(str2));
+    }
+
+    private static String compress(String str) {
+        String res = "";
+        int i = 0;
+        while (i < str.length()) {
+            int j = i, count = 0;
+            while (j < str.length() && str.charAt(i) == str.charAt(j)) {
+                count++;
+                j++;
+            }
+            res += str.charAt(i) + String.valueOf(count);
+            i = j;
+        }
+
+        return res.length() < str.length() ? res : str;
+    }
+}
