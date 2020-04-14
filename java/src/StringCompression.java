d7b5b12ecc26a9215a437e561adcf72a5d510048
vinayak.chaturvedi96@gmail.com <vinayak.chaturvedi96@gmail.com>
2020-04-14 20:02:50 +0530

    Modifying to StringBuilder

--- java/src/StringCompression.java
+++ java/src/StringCompression.java
@@ -8,8 +8,8 @@ public class StringCompression {
         System.out.println("Compression of " + str2 + " is: " + compress(str2));
     }
 
-    private static String compress(String str) {
-        String res = "";
+    private static StringBuilder compress(String str) {
+        StringBuilder res = new StringBuilder();
         int i = 0;
         while (i < str.length()) {
             int j = i, count = 0;
@@ -17,10 +17,10 @@ public class StringCompression {
                 count++;
                 j++;
             }
-            res += str.charAt(i) + String.valueOf(count);
+            res.append(str.charAt(i) + String.valueOf(count));
             i = j;
         }
 
-        return res.length() < str.length() ? res : str;
+        return res.length() < str.length() ? res : new StringBuilder(str);
     }
 }
