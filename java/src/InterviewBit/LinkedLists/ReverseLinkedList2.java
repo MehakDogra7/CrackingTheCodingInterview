dccfbd569c3c4f1a6ebee12c99fe166cf637d9aa
vinayak <vinayak.chaturvedi96@gmail.com>
2020-07-24 20:06:15 +0530

    Adding Code

---
+++ java/src/InterviewBit/LinkedLists/ReverseLinkedList2.java
@@ -0,0 +1,59 @@
+package InterviewBit.LinkedLists;
+
+public class ReverseLinkedList2 {
+
+    public static void main(String[] args) {
+
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        ListNode temp = new ReverseLinkedList2().solve(a, 2, 4);
+
+        while (temp != null) {
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+    }
+
+    private ListNode solve(ListNode A, int B, int C) {
+        if (A == null || A.next == null)
+            return A;
+
+        ListNode result = new ListNode(Integer.MAX_VALUE);
+        result.next = A;
+
+        int x = 1;
+        ListNode start = A;
+        ListNode prev = result;
+        while (start != null && x < B) {
+            prev = start;
+            start = start.next;
+            x++;
+        }
+
+        ListNode old = prev;
+        ListNode curr = start;
+        ListNode next = null;
+
+        while (curr != null && x <= C) {
+            next = curr.next;
+            curr.next = old;
+            old = curr;
+            curr = next;
+            x++;
+        }
+
+        prev.next.next = curr;
+        prev.next = old;
+
+        return result.next;
+    }
+}
