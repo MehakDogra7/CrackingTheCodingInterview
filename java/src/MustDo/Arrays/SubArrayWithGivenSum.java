d31b2f1324346de88fae4c61e1578d921f6d2493
vinayak <vinayak.chaturvedi96@gmail.com>
2020-07-29 21:00:03 +0530

    Adding Code

--- java/src/MustDo/Arrays/SubArrayWithGivenSum.java
+++ java/src/MustDo/Arrays/SubArrayWithGivenSum.java
@@ -1,15 +1,24 @@
 package MustDo.Arrays;
 
+import java.util.HashMap;
+
 public class SubArrayWithGivenSum {
 
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3};
-        int target = 3;
-        //bruteForce(array, target);
+        int[] array = {10, 2, -2, -20, 10};
+        int target = -10;
+        bruteForce(array, target);
         optimal(array, target);
+        optimal2(array, target);
     }
 
+    /**
+     * T.C. : O(n^2)
+     *
+     * @param array
+     * @param target
+     */
     private static void bruteForce(int[] array, int target) {
         boolean flag = false;
         for (int i = 0; i < array.length; i++) {
@@ -30,6 +39,13 @@ public class SubArrayWithGivenSum {
         }
     }
 
+    /**
+     * T.C. : O(n)
+     * Doesn't handle negative values
+     *
+     * @param array
+     * @param target
+     */
     private static void optimal(int[] array, int target) {
         int l = 0;
         int sum = 0;
@@ -48,5 +64,41 @@ public class SubArrayWithGivenSum {
         }
 
         System.out.println("-1");
+    }
+
+    /**
+     * T.C. : O(n)
+     * S.C. : O(n)
+     * Does handle negative values
+     *
+     * @param array
+     * @param target
+     */
+    private static void optimal2(int[] array, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int currSum = 0;
+        int start = 0;
+        int end = -1;
+
+        for (int i = 0; i < array.length; i++) {
+            currSum += array[i];
+
+            if (currSum == target) {
+                start = 0;
+                end = i;
+                break;
+            }
+
+            if (map.containsKey(currSum - target)) {
+                start = map.get(currSum - target) + 1;
+                end = i;
+                break;
+            }
+
+            map.put(currSum, i);
+        }
+
+        System.out.println(start + " " + end);
     }
 }
