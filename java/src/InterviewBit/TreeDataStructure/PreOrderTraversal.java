a938f98df9779a44e5e9821c06ab52bd216c72c6
Vinayak Chaturvedi <vinayak.chaturvedi96@gmail.com>
2020-08-07 14:27:40 +0530

    Adding Tree Code

---
+++ java/src/InterviewBit/TreeDataStructure/PreOrderTraversal.java
@@ -0,0 +1,42 @@
+package InterviewBit.TreeDataStructure;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class PreOrderTraversal {
+
+    public static void main(String[] args) {
+
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(4);
+        TreeNode e = new TreeNode(5);
+        TreeNode f = new TreeNode(6);
+        TreeNode g = new TreeNode(7);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.left = f;
+        c.right = g;
+
+        new PreOrderTraversal().solve(a).forEach(i -> System.out.print(i + " "));
+    }
+
+    private ArrayList<Integer> solve(TreeNode A) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(A);
+
+        while (!stack.isEmpty()) {
+            TreeNode pop = stack.pop();
+            result.add(pop.val);
+            if (pop.right != null) stack.push(pop.right);
+            if (pop.left != null) stack.push(pop.left);
+        }
+
+        return result;
+    }
+}
