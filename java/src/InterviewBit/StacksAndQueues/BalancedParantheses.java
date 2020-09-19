dc52c91a4d700182343c2be641af40494141f376
Vinayak Chaturvedi <vinayak.chaturvedi96@gmail.com>
2020-09-20 00:59:41 +0530

    Adding BalancedParantheses.java

---
+++ java/src/InterviewBit/StacksAndQueues/BalancedParantheses.java
@@ -0,0 +1,27 @@
+package InterviewBit.StacksAndQueues;
+
+import java.util.Stack;
+
+public class BalancedParantheses {
+
+    public static void main(String[] args) {
+        String A = "(()())";
+        System.out.println(new BalancedParantheses().solve(A));
+    }
+
+    public int solve(String A) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < A.length(); i++) {
+            char curr = A.charAt(i);
+            if (curr == '(') stack.push(curr);
+            else {
+                if (stack.isEmpty()) return 0;
+                stack.pop();
+            }
+
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
