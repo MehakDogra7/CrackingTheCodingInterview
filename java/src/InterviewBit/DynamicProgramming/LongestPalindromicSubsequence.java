691f929ca9778d849ea939560daf102cb6c2435d
Vinayak Chaturvedi <vinayak.chaturvedi96@gmail.com>
2020-10-14 01:28:21 +0530

    Adding LongestPalindromicSubsequence.java

--- java/src/InterviewBit/DynamicProgramming/LongestPalindromicSubsequence.java
+++ java/src/InterviewBit/DynamicProgramming/LongestPalindromicSubsequence.java
@@ -25,4 +25,27 @@ public class LongestPalindromicSubsequence {
 
         return dp[n][0];
     }
+
+    public int solve2(String A) {
+        if(A.length() < 2) return A.length();
+
+        int n = A.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++)
+            dp[i][i] = 1;
+
+        int len = 2;
+        for(int x = 1 ; x < n; x++){
+            for (int i = 0; i < n - x; i++){
+                int j = i + len - 1;
+                if(A.charAt(i) == A.charAt(j))
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+            len++;
+        }
+        return dp[0][n-1];
+    }
 }
